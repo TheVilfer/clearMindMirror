d3e5e9c21080c66143da88f6a364c30b8d36f541
Mikita Drazdou <m.drazdou@innopolis.university>
2024-06-12 12:38:54 +0300

    env

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
-
-import config
 from handlers import router
 
 from dotenv import load_dotenv
