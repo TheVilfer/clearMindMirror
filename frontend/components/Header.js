7c829a8f2d04cc855b9aad21994731024e7354be
solncemy <an.ankudinova077@gmail.com>
2024-06-25 11:50:10 +0300

    Add Header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,8 +1,12 @@
 'use client'
+
+import { Button } from "@/components/ui/button"
+
+
 export default function Header() {
     return (
         <>
-
+            <Button variant="outline">Profile</Button>
         </>
     )
 } 
