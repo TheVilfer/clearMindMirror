9ddcb8d0b77b6f235432656f20b2454396f455cb
Sergey Polin <s.polin@innopolis.university>
2024-06-25 11:44:51 +0300

    chore: Add Header component to chat page

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,7 +1,11 @@
 'use client'
+
+import { Button } from "@/components/ui/button"
+
 export default function Header() {
     return (
         <>
+            <Button variant="outline">Button</Button>
 
         </>
     )
