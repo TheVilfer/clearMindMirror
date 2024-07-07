0bf707094a800da7c1a8009ea0ee5fa4a81ca3c1
Sergey Polin <s.polin@innopolis.university>
2024-07-07 17:25:18 +0300

    yakov version

--- frontend/app/api/chat/route.js
+++ frontend/app/api/chat/route.js
@@ -1,41 +1,4 @@
 // // app/api/chat.js
-// import { NextResponse } from "next/server";
-
-
-// export async function POST(req) {
-//   let { message, conversationID } = await req.json();
-//   console.log(message, conversationID)
-
-//   const url = "http://20.98.104.160:8000/invoke";
-//   const cookies = 'user_id=0808080';
-
-//   const response = await fetch(url, {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Cookie': cookies
-//     },
-//     body: JSON.stringify({
-//       input: { human_input: message },
-//       config: {
-//         configurable: {
-//           conversation_id: conversationID
-//         }
-//       }
-//     })
-//   }
-//   )
-
-//   const data = await response.json();
-//   console.log(data);
-
-//   return NextResponse.json({ data }, { status: 200 });
-
-// }
-
-
-
-// app/api/chat.js
 import { NextResponse } from "next/server";
 
 
@@ -43,17 +6,22 @@ export async function POST(req) {
   let { message, conversationID } = await req.json();
   console.log(message, conversationID)
 
-  const url = "http://51.12.240.54:8000/invoke";
+  const url = "http://20.98.104.160:8000/invoke";
+  const cookies = 'user_id=0808080';
 
   const response = await fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      'Cookie': cookies
     },
     body: JSON.stringify({
-      input: { input: message },
-      config: {},
-      kwargs: {}
+      input: { human_input: message },
+      config: {
+        configurable: {
+          conversation_id: conversationID
+        }
+      }
     })
   }
   )
@@ -64,3 +32,35 @@ export async function POST(req) {
   return NextResponse.json({ data }, { status: 200 });
 
 }
+
+
+
+// app/api/chat.js
+// import { NextResponse } from "next/server";
+
+
+// export async function POST(req) {
+//   let { message, conversationID } = await req.json();
+//   console.log(message, conversationID)
+
+//   const url = "http://51.12.240.54:8000/invoke";
+
+//   const response = await fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       input: { input: message },
+//       config: {},
+//       kwargs: {}
+//     })
+//   }
+//   )
+
+//   const data = await response.json();
+//   console.log(data);
+
+//   return NextResponse.json({ data }, { status: 200 });
+
+// }
